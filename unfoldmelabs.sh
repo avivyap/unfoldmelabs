1be85081323531f07462b9bd864a6add1663f598
aviv <141031195+avivyap@users.noreply.github.com>
2023-08-07 17:46:45 +0200

    Add files via upload

---
+++ unfoldmelabs.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+# unfoldmelabs, Author avivyap (@avivyap)
+
+
+
+#Colours
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
+
+echo -ne  "\e[32m Hecho por\e[0m"
+
+echo -e "\e[33m Avivyap\e[0m"
+
+sleep 2
+
+echo -ne "\e[32m Instagram\e[0m"
+
+sleep 2
+
+echo -e "\e[33m @avivyap\e[0m"
+
+sleep 2.5
+
+echo -e "\e[33m[+] Se van a descargar todos los laboratorios\e[0m"
+
+sleep 2.5
+
+git clone https://github.com/jbarone/xxelab
+
+sleep 0.5
+
+git clone https://github.com/appsecco/sqlinjection-training-app
+
+sleep 0.5
+
+wget  https://seedsecuritylabs.org/Labs_20.04/Files/Web_CSRF_Elgg/Labsetup.zip
+
+sleep 0.5
+
+git clone https://github.com/vavkamil/dvwp
+
+sleep 0.5
+
+git clone https://github.com/globocom/secDevLabs
+
+sleep 0.5
+
+echo -e "\e[33m[+] Se han descargado los siguientes laboratorios\e[0m"
+
+sleep 1
+
+echo -e "\e[32m[+] xxelab\e[0m"
+
+sleep 1
+
+echo -e "\e[32m[+] sqlinjection-training-app\e[0m"
+
+sleep 1
+
+echo -e "\e[32m[+] seedsecuritylabs\e[0m"
+
+sleep 1
+
+echo -e "\e[32m[+] dvwp\e[0m"
+
+sleep 1
+
+echo -e "\e[32m[+] secDecLabs\e[0m"
+
+sleep 1
+
+echo -e "\e[33m[+] Con estos laboratorios podras practicar\e[0m"
+
+sleep 1
+
+echo -e "\e[32m[+] xxelab -----------------------------> XXE\e[0m"
+
+sleep 1
+
+echo -e "\e[32m[+] sqlinjection-training-app ----------> SQLI\e[0m"
+
+sleep 1
+
+echo -e "\e[32m[+] seedsecuritylabs -------------------> CSRF\e[0m"
+
+sleep 1
+
+echo -e "\e[32m[+] dvwp -------------------------------> RFI\e[0m"
+
+sleep 1
+
+echo -e "\e[32m[+] secDecLabs -------------------------> XSS\e[0m"
+
+sleep 3
+
+echo -e "\e[36mDisfruta ;)\e[0m"
+
+
+
+
+
+
+
